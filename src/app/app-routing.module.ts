69ba2e35c0f0a2d129678e4a40de7214d74fc848
Parth Shah <parthshah0801@gmail.com>
2022-06-14 17:36:53 +0530

    admin added in routing

--- src/app/app-routing.module.ts
+++ src/app/app-routing.module.ts
@@ -1,7 +1,10 @@
 import { NgModule } from '@angular/core';
 import { RouterModule, Routes } from '@angular/router';
+import { AdminhomepageComponent } from './components/admin/adminhomepage/adminhomepage.component';
 
-const routes: Routes = [];
+const routes: Routes = [
+  {path:"admin",component:AdminhomepageComponent}
+];
 
 @NgModule({
   imports: [RouterModule.forRoot(routes)],
