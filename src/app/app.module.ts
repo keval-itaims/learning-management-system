da843092b5dac7e51c76c8359978216724c24a51
Keval Shah <kevalshah2161@gmail.com>
2022-06-14 12:20:44 +0530

    new commponents

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -3,10 +3,14 @@ import { BrowserModule } from '@angular/platform-browser';
 
 import { AppRoutingModule } from './app-routing.module';
 import { AppComponent } from './app.component';
+import { NavbarComponent } from './components/homepage/navbar/navbar.component';
+import { FooterComponent } from './components/homepage/footer/footer.component';
 
 @NgModule({
   declarations: [
-    AppComponent
+    AppComponent,
+    NavbarComponent,
+    FooterComponent
   ],
   imports: [
     BrowserModule,
