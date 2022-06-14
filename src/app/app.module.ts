12fac6d7a179eaf574fb1833c64762f2c4400d31
Keval Shah <kevalshah2161@gmail.com>
2022-06-14 15:02:25 +0530

    assets

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -6,12 +6,18 @@ import { AppComponent } from './app.component';
 import { NavbarComponent } from './components/homepage/navbar/navbar.component';
 import { FooterComponent } from './components/homepage/footer/footer.component';
 import { AdminComponent } from './components/adminpage/admin/admin.component';
+import { HomeComponent } from './components/homepage/home/home.component';
+import { TestimonialsComponent } from './components/homepage/testimonials/testimonials.component';
+import { InstructorsComponent } from './components/homepage/instructors/instructors.component';
 @NgModule({
   declarations: [
     AppComponent,
     NavbarComponent,
     FooterComponent,
     AdminComponent,
+    HomeComponent,
+    TestimonialsComponent,
+    InstructorsComponent,
 ],
   imports: [
     BrowserModule,
