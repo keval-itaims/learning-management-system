93c7d80b80d49613055add110a4db5587e57d5ab
Parth Shah <parthshah0801@gmail.com>
2022-06-14 17:28:40 +0530

    admin

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -5,29 +5,32 @@ import { AppRoutingModule } from './app-routing.module';
 import { AppComponent } from './app.component';
 import { NavbarComponent } from './components/homepage/navbar/navbar.component';
 import { FooterComponent } from './components/homepage/footer/footer.component';
-import { AdminComponent } from './components/adminpage/admin/admin.component';
+// import { AdminComponent } from './components/adminpage/admin/admin.component';
 import { HomeComponent } from './components/homepage/home/home.component';
 import { TestimonialsComponent } from './components/homepage/testimonials/testimonials.component';
 import { InstructorsComponent } from './components/homepage/instructors/instructors.component';
 import { HeroComponent } from './components/homepage/hero/hero.component';
 import { AdminhomepageComponent } from './components/admin/adminhomepage/adminhomepage.component';
 import { BrowserAnimationsModule } from '@angular/platform-browser/animations';
+import { MaterialModule } from './material/material.module';
 @NgModule({
   declarations: [
     AppComponent,
     NavbarComponent,
     FooterComponent,
-    AdminComponent,
+    // AdminComponent,
     HomeComponent,
     TestimonialsComponent,
     InstructorsComponent,
     HeroComponent,
     AdminhomepageComponent,
+
 ],
   imports: [
     BrowserModule,
     AppRoutingModule,
-    BrowserAnimationsModule
+    BrowserAnimationsModule,
+    MaterialModule
   ],
   providers: [],
   bootstrap: [AppComponent]
