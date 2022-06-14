6427dd9d885b84bbdb323a8e882645d2af45370f
Keval Shah <kevalshah2161@gmail.com>
2022-06-14 21:49:49 +0530

    new components

--- src/app/app-routing.module.ts
+++ src/app/app-routing.module.ts
@@ -3,12 +3,16 @@ import { RouterModule, Routes } from '@angular/router';
 import { AboutComponent } from './components/homepage/about/about.component';
 import { ContactComponent } from './components/homepage/contact/contact.component';
 import { ErrorComponent } from './components/homepage/error/error.component';
-import { HomeComponent } from './components/homepage/home/home.component';
 import { AdminhomepageComponent } from './components/admin/adminhomepage/adminhomepage.component';
+import { HomepageComponent } from './components/homepage/homepage/homepage.component';
+import { MainComponent } from './components/homepage/main/main.component';
 
 const routes: Routes = [
-  {path:'', redirectTo:'home', pathMatch: 'full'},
-  {path:'home', component:HomeComponent},
+  {path:'', redirectTo:'homepage', pathMatch: 'full'},
+  {path:'homepage', component:HomepageComponent, children:[
+    {path: '', redirectTo: 'main', pathMatch: 'full'},
+    {path: 'main', component:MainComponent},
+  ]},
   {path:'about', component:AboutComponent},
   {path:'contact', component:ContactComponent},
   {path:'**', component:ErrorComponent},
--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -5,8 +5,6 @@ import { AppRoutingModule } from './app-routing.module';
 import { AppComponent } from './app.component';
 import { NavbarComponent } from './components/homepage/navbar/navbar.component';
 import { FooterComponent } from './components/homepage/footer/footer.component';
-// import { AdminComponent } from './components/adminpage/admin/admin.component';
-import { HomeComponent } from './components/homepage/home/home.component';
 import { TestimonialsComponent } from './components/homepage/testimonials/testimonials.component';
 import { InstructorsComponent } from './components/homepage/instructors/instructors.component';
 import { HeroComponent } from './components/homepage/hero/hero.component';
@@ -19,13 +17,13 @@ import { FontAwesomeModule } from '@fortawesome/angular-fontawesome';
 import { AdminhomepageComponent } from './components/admin/adminhomepage/adminhomepage.component';
 import { BrowserAnimationsModule } from '@angular/platform-browser/animations';
 import { MaterialModule } from './material/material.module';
+import { HomepageComponent } from './components/homepage/homepage/homepage.component';
+import { MainComponent } from './components/homepage/main/main.component';
 @NgModule({
   declarations: [
     AppComponent,
     NavbarComponent,
     FooterComponent,
-    // AdminComponent,
-    HomeComponent,
     TestimonialsComponent,
     InstructorsComponent,
     HeroComponent,
@@ -35,6 +33,8 @@ import { MaterialModule } from './material/material.module';
     ContactComponent,
     ErrorComponent,
     AdminhomepageComponent,
+    HomepageComponent,
+    MainComponent,
 ],
   imports: [
     BrowserModule,
