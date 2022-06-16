c8f417f815c31f04f57605e3ea0343cba2736e88
sunny-aims <107387534+sunny-aims@users.noreply.github.com>
2022-06-16 17:36:05 +0530

    Edited signup component
    Added code for form and field validation

--- src/app/components/pages/signup/signup.component.ts
+++ src/app/components/pages/signup/signup.component.ts
@@ -1,4 +1,10 @@
 import { Component, OnInit } from '@angular/core';
+import {
+  FormBuilder,
+  FormControl,
+  FormGroup,
+  Validators,
+} from '@angular/forms';
 
 @Component({
   selector: 'app-signup',
@@ -6,10 +12,61 @@ import { Component, OnInit } from '@angular/core';
   styleUrls: ['./signup.component.css']
 })
 export class SignupComponent implements OnInit {
+  
+  firstName: FormControl;
+  lastName: FormControl;
+  email: FormControl;
+  password: FormControl;
+  rep_password: FormControl;
 
-  constructor() { }
+  regForm: FormGroup;
 
-  ngOnInit(): void {
+  //patterns
+  password_pattern =
+    '^(?=.*[a-z])(?=.*[A-Z])(?=.*d)(?=.*[@$!%*?&])[A-Za-zd@$!%*?&]{6,}$';
+  pat_email = '[a-z0-9._%+-]+@[a-z0-9.-]+\\.[a-z]{2,4}';
+  name_pattern = '[A-Za-z ]+';
+
+  show_pass = false; //Toggle between show & hide password
+  match_password_error = false; //To show error of matching password
+
+  constructor(private fb: FormBuilder) {}
+
+  ngOnInit() {
+    this.regForm = this.fb.group({
+      firstName: [
+        '',
+        [Validators.required, Validators.pattern(this.name_pattern)],
+      ],
+      lastName: [
+        '',
+        [Validators.required, Validators.pattern(this.name_pattern)],
+      ],
+      email: ['', [Validators.required, Validators.pattern(this.pat_email)]],
+      password: [
+        '',
+        [Validators.required, Validators.pattern(this.password_pattern)],
+      ],
+      rep_password: ['', [Validators.required]],
+    });
+    console.log('Registration Form Created');
   }
+
+  check_password() {
+    let pass = this.regForm.controls['password'].value;
+    let rep_pass = this.regForm.controls['rep_password'].value;
+
+    if (pass === rep_pass) {
+      this.match_password_error = false;
+    } else {
+      this.match_password_error = true;
+    }
+  }
+
+  togglePassword() {
+    console.log('toggle password');
+    this.show_pass = !this.show_pass;
+  }
+
 
 }
