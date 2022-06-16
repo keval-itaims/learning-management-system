fddfe0a9728384e4090831e0cf0bffe2b38a636f
Keval Shah <kevalshah2161@gmail.com>
2022-06-16 23:20:13 +0530

    footer

--- src/app/components/homepage/footer/footer.component.ts
+++ src/app/components/homepage/footer/footer.component.ts
@@ -1,12 +1,15 @@
 import { Component, OnInit } from '@angular/core';
-
+import {faLocationDot, faMessage, faPhone, faArrowRightLong} from '@fortawesome/free-solid-svg-icons'
 @Component({
   selector: 'app-footer',
   templateUrl: './footer.component.html',
   styleUrls: ['./footer.component.css']
 })
 export class FooterComponent implements OnInit {
-
+  faLocation = faLocationDot;
+  faMessage = faMessage;
+  faPhone = faPhone;
+  faArrow = faArrowRightLong;
   constructor() { }
 
   ngOnInit(): void {
