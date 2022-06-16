8d246e3a7d2f1e0ec436d54ca7018f6d6c84142a
sunny-aims <107387534+sunny-aims@users.noreply.github.com>
2022-06-16 12:56:32 +0530

    User Login Credential
    User Login Credential Added

--- src/app/classes/user-login.ts
+++ src/app/classes/user-login.ts
@@ -1,2 +1,10 @@
 export class UserLogin {
+  email: string;
+  password: string;
+
+  constructor(email: string, password: string) {
+    console.log('LoginCred Created...');
+    this.email = email;
+    this.password = password;
+  }
 }
