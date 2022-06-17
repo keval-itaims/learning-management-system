224e6faad27a74871f4da8af84d8109ed11b1a01
Parth Shah <parthshah0801@gmail.com>
2022-06-17 09:08:35 +0530

    working on add instructors

--- src/app/material/material.module.ts
+++ src/app/material/material.module.ts
@@ -7,6 +7,7 @@ import {MatSelectModule} from '@angular/material/select';
 import {MatProgressBarModule} from '@angular/material/progress-bar';
 import {MatTableModule} from '@angular/material/table';
 import {MatFormFieldModule} from '@angular/material/form-field';
+import {MatInputModule} from '@angular/material/input';
 
 
 
@@ -18,7 +19,8 @@ const MaterialComponents =[
   MatSelectModule,
   MatProgressBarModule,
   MatTableModule,
-  MatFormFieldModule
+  MatFormFieldModule,
+  MatInputModule
 ]
 @NgModule({
   declarations: [],
