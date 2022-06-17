b5a7a101fe3c33311077cabb2a8c1264a2b5b52e
Keval Shah <kevalshah2161@gmail.com>
2022-06-17 10:57:55 +0530

    import

--- src/app/app-routing.module.ts
+++ src/app/app-routing.module.ts
@@ -6,9 +6,10 @@ import { ErrorComponent } from './components/pages/error/error.component';
 import { AdminhomepageComponent } from './components/admin/adminhomepage/adminhomepage.component';
 import { HomepageComponent } from './components/homepage/homepage/homepage.component';
 import { MainComponent } from './components/homepage/main/main.component';
-// import { SignupComponent } from './components/pages/signup/signup.component';
 import { LoginComponent } from './components/pages/login/login.component';
 import { AddinstructorComponent } from './components/admin/instructor/addinstructor/addinstructor.component';
+import { SignupComponent } from './components/pages/signup/signup.component';
+import { CoursesComponent } from './components/coursespage/courses/courses.component';
 
 const routes: Routes = [
   {path:'', redirectTo:'homepage', pathMatch: 'full'},
@@ -20,7 +21,8 @@ const routes: Routes = [
     {path:'about', component:AboutComponent},
     {path:'contact', component:ContactComponent},
     {path:'error', component:ErrorComponent},
-    // {path:'signup', component:SignupComponent},
+    {path:'signup', component:SignupComponent},
+    {path:'courses', component:CoursesComponent},
     {path:'login', component:LoginComponent},
   ]},
   {path:'**', redirectTo:'homepage/error'},
