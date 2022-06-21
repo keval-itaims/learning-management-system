841f4648768272531125cd584f8882acc6f5d48c
Keval Shah <kevalshah2161@gmail.com>
2022-06-21 10:01:31 +0530

    my account

--- src/app/components/pages/account/myaccount/myaccount.component.ts
+++ src/app/components/pages/account/myaccount/myaccount.component.ts
@@ -4,16 +4,35 @@ import {
   FormGroup,
   Validators,
 } from '@angular/forms';
+import { User } from 'src/app/classes/user';
 @Component({
   selector: 'app-myaccount',
   templateUrl: './myaccount.component.html',
   styleUrls: ['./myaccount.component.css']
 })
 export class MyaccountComponent implements OnInit {
-
-  constructor() { }
+  user:User = new User()
+  userForm: FormGroup | any;
+  name_pattern = '[A-Za-z ]+';
+  constructor(private fb: FormBuilder) {}
 
   ngOnInit(): void {
+    this.userForm = this.fb.group({
+      firstName: [
+        this.user.firstName,
+        [Validators.required, Validators.pattern(this.name_pattern)],
+      ],
+      lastName: [
+        this.user.lastName,
+        [Validators.required, Validators.pattern(this.name_pattern)],
+      ],
+      email: [
+        this.user.email,
+      ],
+  })
   }
-
+  onSubmit(){
+    this.user.firstName = this.userForm.firstName;
+    this.user.lastName = this.userForm.lastName;
+  }
 }
