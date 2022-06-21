b07885ba47f1a6378deb5266312acad37296f349
Keval Shah <kevalshah2161@gmail.com>
2022-06-21 19:02:16 +0530

    login

--- src/app/services/login.service.ts
+++ src/app/services/login.service.ts
@@ -1,9 +1,22 @@
+import { HttpClient } from '@angular/common/http';
 import { Injectable } from '@angular/core';
+import { Observable } from 'rxjs';
+import { User } from '../classes/user';
 
 @Injectable({
   providedIn: 'root'
 })
 export class LoginService {
+  url = 'http://localhost:8080/login'
+  constructor(private http: HttpClient) { }
+  login(user:User){
+    this.doLogin(user).subscribe(
+      (data)=>{
 
-  constructor() { }
+      }
+    )
+  }
+  doLogin(user:User): Observable<any>{
+    return this.http.post<any>(this.url, user);
+  }
 }
