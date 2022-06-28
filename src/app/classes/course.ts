c8c533dbe98c02e3621d0ce70fe05586105b140a
Keval Shah <kevalshah2161@gmail.com>
2022-06-28 18:41:44 +0530

    course

---
+++ src/app/classes/course.ts
@@ -0,0 +1,12 @@
+export class Course{
+    courseId!: number;
+    courseName!: string;
+    courseDescription!: string;
+    coursePrice!: number;
+    courseDuration!: number;
+    courseDate!: Date;
+    courseTutorId!: number;
+    courseRating!: number;
+    courseStatus!: string;
+    courseImage!: any;
+}
