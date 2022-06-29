464a0f1220c929030b1e28914ef293d8b8d2f439
Parth Shah <parthshah0801@gmail.com>
2022-06-29 19:16:58 +0530

    contactus functionality complete

--- src/app/classes/contact.ts
+++ src/app/classes/contact.ts
@@ -4,4 +4,5 @@ export class Contact {
     emailId!: string;
     message!: string;
     status!: number;
+    replyMessage!:string
 }
--- src/app/components/admin/contact-us/replycontactmessage/replycontactmessage.component.ts
+++ src/app/components/admin/contact-us/replycontactmessage/replycontactmessage.component.ts
@@ -1,5 +1,8 @@
 import { Component, OnInit } from '@angular/core';
-import { ActivatedRoute } from '@angular/router';
+import { FormControl, FormGroup, Validators } from '@angular/forms';
+import { ActivatedRoute, Router } from '@angular/router';
+import { UtilityService } from 'src/app/services/utility.service';
+import { Contact } from '../../../../classes/contact';
 
 @Component({
   selector: 'app-replycontactmessage',
@@ -8,11 +11,65 @@ import { ActivatedRoute } from '@angular/router';
 })
 export class ReplycontactmessageComponent implements OnInit {
 
-  constructor(private activateRouter:ActivatedRoute) { }
+  constructor(private activateRouter:ActivatedRoute,private utilityService:UtilityService,private router:Router) { }
   id!:number
+  contactMessage!:Contact;
+  replyMessageForm !: FormGroup;
+  isLoading : boolean = true
+
   ngOnInit(): void {
-    this.id = this.activateRouter.snapshot.params['id'];
-    console.log(this.id)
+
+    this.replyMessageForm = new FormGroup({
+      "name" : new FormControl(''),
+      "emailId" : new FormControl(''),
+      "message" : new FormControl(''),
+      "replyMessage" : new FormControl('',[Validators.required])
+
+    })
+    this.getContactMessage();
   }
 
+  getContactMessage(){
+    this.id = this.activateRouter.snapshot.params['id'];
+    console.log("id",this.id);
+    this.utilityService.getContactMessageById(this.id).subscribe(
+      data => {
+
+        console.log("Data : ",data),
+        this.contactMessage = data;
+        console.log("Object",this.contactMessage)
+        this.replyMessageForm.patchValue({
+          name : this.contactMessage.name,
+          emailId : this.contactMessage.emailId,
+          message : this.contactMessage.message
+        })
+        this.isLoading = false;
+
+    },
+      error => {
+        this.isLoading = false;
+        console.log(error)
+      }
+    )
+  }
+
+  onReply(){
+    this.contactMessage = this.replyMessageForm.value;
+    this.contactMessage.cId = this.id;
+    console.log(this.contactMessage);
+
+    this.utilityService.replyContactMessage(this.contactMessage).subscribe(
+      data => {
+        this.utilityService.openSnackBar("Mail send!","close");
+        this.router.navigate(['/admin/contact-us/detail'])
+        console.log(data)
+      },
+      error => console.log(error)
+    )
+  }
+
+  get name(){return this.replyMessageForm.get('name')}
+  get emailId(){return this.replyMessageForm.get('emailId')}
+  get message(){return this.replyMessageForm.get('message')}
+  get replymessage(){return this.replyMessageForm.get('replyMessage')}
 }
