e974eec741c89f9bd4fe7473fc5baa01936d69e7
Keval Shah <kevalshah2161@gmail.com>
2022-07-07 16:33:16 +0530

    some changes

--- src/app/components/homepage/testcourses/testcourse/testcourse.component.ts
+++ src/app/components/homepage/testcourses/testcourse/testcourse.component.ts
@@ -1,5 +1,7 @@
 import { Component, OnInit, Input } from '@angular/core';
+import { ActivatedRoute, Router } from '@angular/router';
 import {faStar, faClock} from '@fortawesome/free-solid-svg-icons';
+import { CourseResponse } from 'src/app/classes/course-response';
 
 @Component({
   selector: 'app-testcourse',
@@ -9,10 +11,14 @@ import {faStar, faClock} from '@fortawesome/free-solid-svg-icons';
 export class TestcourseComponent implements OnInit {
   faStar = faStar;
   faClock = faClock;
-  @Input() course:any;
-  constructor() { }
+  @Input() course:any | CourseResponse;
+  constructor(private router: Router,
+    private route: ActivatedRoute) { }
 
   ngOnInit(): void {    
+  }
+  courseClick(){
+    this.router.navigate([`../course/${this.course.courseId}`], {relativeTo:this.route})
   }
 
 }
