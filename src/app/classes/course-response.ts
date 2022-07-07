8e03354330c0fda4efc052dd1e1dbfe12435014f
Keval Shah <kevalshah2161@gmail.com>
2022-07-07 14:03:00 +0530

    courses done

--- src/app/classes/course-response.ts
+++ src/app/classes/course-response.ts
@@ -1,3 +1,5 @@
+import { Chapters } from "./chapters";
+
 export class CourseResponse{
     courseId!: number;
     courseName!: string;
@@ -10,4 +12,5 @@ export class CourseResponse{
     courseImage!: string;
     tutorName!: string;
     tutorImage!: string;
+    chapters!: Chapters[]
 }
