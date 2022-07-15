5b6a7cc5685c0e8e70f586f64ad40242c38fa3f8
Keval Shah <kevalshah2161@gmail.com>
2022-07-15 16:38:33 +0530

    push

--- src/app/components/admin/blog/add-blog/add-blog.component.ts
+++ src/app/components/admin/blog/add-blog/add-blog.component.ts
@@ -10,7 +10,7 @@ import { Component, OnInit } from '@angular/core';
 })
 export class AddBlogComponent implements OnInit {
 
-  // Editor = ClassicEditor;
+  // public Editor = ClassicEditor;
   constructor() { }
 
   ngOnInit(): void {
