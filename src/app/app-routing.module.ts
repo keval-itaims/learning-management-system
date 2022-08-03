04e7a3547127f47f433ecc552053c01141ce4e7f
Keval Shah <kevalshah2161@gmail.com>
2022-08-03 12:24:59 +0530

    blog

--- src/app/app-routing.module.ts
+++ src/app/app-routing.module.ts
@@ -15,7 +15,7 @@ import { UpdateInstructorComponent } from './components/admin/instructor/update-
 import { AccountComponent } from './components/pages/account/account.component';
 import { MyaccountComponent } from './components/pages/account/myaccount/myaccount.component';
 import { MycoursesComponent } from './components/pages/account/mycourses/mycourses.component';
-import { DashboardComponent } from './components/admin/dashboard/dashboard.component'
+import { DashboardComponent } from './components/admin/dashboard/dashboard.component';
 import { InstructorHomepageComponent } from './components/admin/instructor/instructor-homepage/instructor-homepage.component';
 import { StudentComponent } from './components/admin/student/student.component';
 import { AddcontactusComponent } from './components/admin/contact-us/addcontactus/addcontactus.component';
@@ -44,75 +44,115 @@ import { UpdateBlogComponent } from './components/admin/blog/update-blog/update-
 import { BlogsComponent } from './components/pages/blogs/blogs.component';
 import { StudentDetailsComponent } from './components/admin/admincourses/student-details/student-details.component';
 import { InstructorCourseComponent } from './components/admin/instructor/instructor-course/instructor-course.component';
-
+import { ViewblogComponent } from './components/pages/viewblog/viewblog.component';
 
 const routes: Routes = [
-  {path:'', redirectTo:'homepage', pathMatch: 'full'},
-  {path:'create-instructor',component:AddinstructorComponent},
-  {path:'addcontactus',component:AddcontactusComponent},
-  {path:'admin', component:AdminhomepageComponent, children:[
-    {path:'',redirectTo: 'dashboard',pathMatch: 'full'},
-    {path:'dashboard',component:DashboardComponent},
-    {path:'student',component:StudentComponent},
-    {path:'instructor',component:InstructorHomepageComponent,children:[
-        {path:'',redirectTo: 'detail',pathMatch: 'full'},
-        {path:'detail',component:InstructorDetailComponent},
-        {path:'create',component:AddinstructorComponent},
-        {path:'update/:id',component:UpdateInstructorComponent},
-        {path:'instructorcourse/:id',component:InstructorCourseComponent}
-    ]},
-    {path:'contact-us',component:ContactusHomepageComponent,children:[
-      {path:'',redirectTo:'detail',pathMatch:'full'},
-      {path:'detail',component:ContactusDetailComponent},
-      {path:'add',component:AddcontactusComponent},
-      {path:'reply/:id',component:ReplycontactmessageComponent}
-    ]},
-    {path:'courses',component:AdmincoursesComponent,children:[
-      {path:'',redirectTo:'detail',pathMatch:'full'},
-      {path:'create',component:AddcoursesComponent},
-      {path:'view/:id',component:ViewCourseComponent},
-      {path:'detail',component:CoursesDetailsComponent},
-      {path:'update/:id',component:UpdateCourseComponent},
-      {path:'students/:courseId',component:StudentDetailsComponent},
-      {path:'chapter',component:AdminchaptersComponent,children:[
-        {path:'',redirectTo:'detail',pathMatch:'full'},
-        {path:'add/:id',component:AddchapterComponent},
-        {path:'update/:id',component:UpdateChapterComponent},
-        {path:'detail/:id',component:ChapterDetailsComponent}
-      ]}
-    ]},
-    {path:'blog',component:BlogComponent,children:[
-      {path:'',redirectTo:'detail',pathMatch:'full'},
-      {path:'add',component:AddBlogComponent},
-      {path:'detail',component:BlogDetailsComponent},
-      {path:'view/:id',component:ViewBlogComponent},
-      {path:'update/:id',component:UpdateBlogComponent}
-    ]}
-  ]},
-  {path:'homepage', component:HomepageComponent, children:[
-    {path: '', redirectTo: 'main', pathMatch: 'full'},
-    {path: 'main', component:MainComponent, resolve:{courses: HomepageResolver}},
-    {path:'about', component:AboutComponent},
-    {path:'contact', component:ContactComponent},
-    {path:'faqs', component:FaqsComponent},
-    {path:'error', component:ErrorComponent},
-    {path:'signup', component:SignupComponent},
-    {path:'courses', component:CoursesComponent},
-    {path:'course/:id', component:CourseDetailsComponent},
-    {path:'login', component:LoginComponent},
-    {path:'forgot-password', component:ForgotPasswordComponent},
-    {path:'blogs',component:BlogsComponent},
-    {path:'account', component:AccountComponent, children:[
-      {path:'', redirectTo:'myaccount', pathMatch: 'full'},
-      {path:'myaccount', component:MyaccountComponent},
-      {path:'mycourses', component:MycoursesComponent},
-    ]},
-  ]},
-  {path:'**', redirectTo:'homepage/error'},
+  { path: '', redirectTo: 'homepage', pathMatch: 'full' },
+  { path: 'create-instructor', component: AddinstructorComponent },
+  { path: 'addcontactus', component: AddcontactusComponent },
+  {
+    path: 'admin',
+    component: AdminhomepageComponent,
+    children: [
+      { path: '', redirectTo: 'dashboard', pathMatch: 'full' },
+      { path: 'dashboard', component: DashboardComponent },
+      { path: 'student', component: StudentComponent },
+      {
+        path: 'instructor',
+        component: InstructorHomepageComponent,
+        children: [
+          { path: '', redirectTo: 'detail', pathMatch: 'full' },
+          { path: 'detail', component: InstructorDetailComponent },
+          { path: 'create', component: AddinstructorComponent },
+          { path: 'update/:id', component: UpdateInstructorComponent },
+          {
+            path: 'instructorcourse/:id',
+            component: InstructorCourseComponent,
+          },
+        ],
+      },
+      {
+        path: 'contact-us',
+        component: ContactusHomepageComponent,
+        children: [
+          { path: '', redirectTo: 'detail', pathMatch: 'full' },
+          { path: 'detail', component: ContactusDetailComponent },
+          { path: 'add', component: AddcontactusComponent },
+          { path: 'reply/:id', component: ReplycontactmessageComponent },
+        ],
+      },
+      {
+        path: 'courses',
+        component: AdmincoursesComponent,
+        children: [
+          { path: '', redirectTo: 'detail', pathMatch: 'full' },
+          { path: 'create', component: AddcoursesComponent },
+          { path: 'view/:id', component: ViewCourseComponent },
+          { path: 'detail', component: CoursesDetailsComponent },
+          { path: 'update/:id', component: UpdateCourseComponent },
+          { path: 'students/:courseId', component: StudentDetailsComponent },
+          {
+            path: 'chapter',
+            component: AdminchaptersComponent,
+            children: [
+              { path: '', redirectTo: 'detail', pathMatch: 'full' },
+              { path: 'add/:id', component: AddchapterComponent },
+              { path: 'update/:id', component: UpdateChapterComponent },
+              { path: 'detail/:id', component: ChapterDetailsComponent },
+            ],
+          },
+        ],
+      },
+      {
+        path: 'blog',
+        component: BlogComponent,
+        children: [
+          { path: '', redirectTo: 'detail', pathMatch: 'full' },
+          { path: 'add', component: AddBlogComponent },
+          { path: 'detail', component: BlogDetailsComponent },
+          { path: 'view/:id', component: ViewBlogComponent },
+          { path: 'update/:id', component: UpdateBlogComponent },
+        ],
+      },
+    ],
+  },
+  {
+    path: 'homepage',
+    component: HomepageComponent,
+    children: [
+      { path: '', redirectTo: 'main', pathMatch: 'full' },
+      {
+        path: 'main',
+        component: MainComponent,
+        resolve: { courses: HomepageResolver },
+      },
+      { path: 'about', component: AboutComponent },
+      { path: 'contact', component: ContactComponent },
+      { path: 'faqs', component: FaqsComponent },
+      { path: 'error', component: ErrorComponent },
+      { path: 'signup', component: SignupComponent },
+      { path: 'courses', component: CoursesComponent },
+      { path: 'course/:id', component: CourseDetailsComponent },
+      { path: 'login', component: LoginComponent },
+      { path: 'forgot-password', component: ForgotPasswordComponent },
+      { path: 'blogs', component: BlogsComponent },
+      { path: 'viewblog/:id', component: ViewblogComponent },
+      {
+        path: 'account',
+        component: AccountComponent,
+        children: [
+          { path: '', redirectTo: 'myaccount', pathMatch: 'full' },
+          { path: 'myaccount', component: MyaccountComponent },
+          { path: 'mycourses', component: MycoursesComponent },
+        ],
+      },
+    ],
+  },
+  { path: '**', redirectTo: 'homepage/error' },
 ];
 
 @NgModule({
-  imports: [RouterModule.forRoot(routes, {scrollPositionRestoration: 'top'})],
-  exports: [RouterModule]
+  imports: [RouterModule.forRoot(routes, { scrollPositionRestoration: 'top' })],
+  exports: [RouterModule],
 })
-export class AppRoutingModule { }
+export class AppRoutingModule {}
